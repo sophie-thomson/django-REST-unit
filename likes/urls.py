2899007ed9c7bc7e016119d02fdfb64cd71cf693
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-19 14:12:18 +0000

    AddLikeList view and LikeDetail view using generics and update urls.py

--- likes/urls.py
+++ likes/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from likes import views
+from django.urls import path
+from likes import views
 
-# urlpatterns = [
-#     path('comments/', views.LikeList.as_view()),
-#     path('comments/<int:pk>/', views.LikeDetail.as_view())
-# ]
+urlpatterns = [
+    path('likes/', views.LikeList.as_view()),
+    path('likes/<int:pk>/', views.LikeDetail.as_view()),
+]
