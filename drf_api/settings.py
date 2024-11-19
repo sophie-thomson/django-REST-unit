340c0761cc10b78796d44220f022fef6bf0bf836
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-19 13:58:23 +0000

    Add likes app and model

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'posts',
     'comments',
+    'likes',
 ]
 
 MIDDLEWARE = [
---
+++ likes/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from likes import views
+
+# urlpatterns = [
+#     path('comments/', views.LikeList.as_view()),
+#     path('comments/<int:pk>/', views.LikeDetail.as_view())
+# ]
